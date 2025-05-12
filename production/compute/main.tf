38d84a1ed1a75bfb430fb25032c9b9d230e5da93
Anthony Hernandez <59671245+ahernandez-recurly@users.noreply.github.com>
2025-05-12 08:34:00 -0700

    Update main.tf

--- production/compute/main.tf
+++ production/compute/main.tf
@@ -2,7 +2,7 @@ module "production_compute_instance" {
   source = "../../modules/compute"
 
   instance_count = 1
-  instance_name  = "production-instance"
+  instance_name  = "production-instaance"
   instance_zone  = "us-central1-a"
   boot_disk_image = "debian-cloud/debian-11"
   network_name    = "default"
