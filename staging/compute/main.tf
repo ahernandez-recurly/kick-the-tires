bddaef49110a0e76ec4a9320545cc64614590406
Anthony Hernandez <59671245+ahernandez-recurly@users.noreply.github.com>
2025-05-12 08:03:35 -0700

    Update main.tf

--- staging/compute/main.tf
+++ staging/compute/main.tf
@@ -2,7 +2,7 @@ module "staging_compute_instance" {
   source = "../../modules/compute"
 
   instance_count = 0
-  instance_name  = "istagingawdadaw-instance"
+  instance_name  = "istaggawdadaw-instance"
   instance_zone  = "us-central1-a"
   boot_disk_image = "debian-cloud/debian-11"
   network_name    = "default"
